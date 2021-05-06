8b11421749c7afd3335feda956c36bd899fa9f31
adavis1989 <83476184+adavis1989@users.noreply.github.com>
2021-05-06 14:45:35 -0500

    added movement

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float _speed = 3.5f;
 
-    // Update is called once per frame
+    
+
+    void Start() =>
+
+        transform.position = new Vector3(0, 0, 0);
+
+
+    
     void Update()
     {
-        
+        CalculateMovement();
+    }
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * VerticalInput * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
+
