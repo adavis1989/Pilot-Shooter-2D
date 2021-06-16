8f55e18727cb6cf0554e95575073b3648bc0aa58
adavis1989 <83476184+adavis1989@users.noreply.github.com>
2021-06-16 16:53:52 -0500

    Added Shield Variation and fixed thruster bug

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50358af50e24780492c329c3262266af14d61a2767fda3baefd9d2f8cfe7d85d
+oid sha256:b244c2674ba147feb1cb3e5d1241c455b0d4de785f938fb10f18d59c8ce626c6
 size 1644
